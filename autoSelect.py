f579624c44e854136cf22db0bdf434ee63026125
brennan <brennanmk2200@gmail.com>
2019-11-28 09:57:34 -0500

    added error correction

--- autoSelect.py
+++ autoSelect.py
@@ -10,43 +10,44 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 
 driver.get("https://leopardweb.wit.edu")
 print("Headless Driver Started")
-counter = 0
+counter = 1
 
-while counter < 3:
-    # Input login and requested CRN numbers
-    username = "millerklugmanb"
-    password = "#Bp02052"
-    CRN1 = "22480"
-    CRN2 = ""
-    CRN3 = ""
-    CRN4 = ""
-    CRN5 = ""
-    CRN6 = ""
+# Input login and requested CRN numbers
+username = "millerklugmanb"
+password = "#Bp02052"
+CRN1 = "22480"
+CRN2 = ""
+CRN3 = ""
+CRN4 = ""
+CRN5 = ""
+CRN6 = ""
 
-    # Find username and enter username
-    id_box = driver.find_element_by_name('username')
-    id_box.send_keys(username)
-    # Find password and enter password
-    pass_box = driver.find_element_by_name('password')
-    pass_box.send_keys(password)
-    # Submit password
-    login_button = driver.find_element_by_name('submit').click()
+# Find username and enter username
+id_box = driver.find_element_by_name('username')
+id_box.send_keys(username)
+# Find password and enter password
+pass_box = driver.find_element_by_name('password')
+pass_box.send_keys(password)
+# Submit password
+login_button = driver.find_element_by_name('submit').click()
 
-    # Navigate to student
-    student_button = driver.find_element_by_link_text('Student').click()
+# Navigate to student
+student_button = driver.find_element_by_link_text('Student').click()
 
-    # Navigate to regristration
-    student_button = driver.find_element_by_link_text('Registration').click()
+# Navigate to regristration
+student_button = driver.find_element_by_link_text('Registration').click()
 
-    # Navigate to select term
-    sub_button = driver.find_element_by_link_text('Select Term').click()
+# Navigate to select term
+sub_button = driver.find_element_by_link_text('Select Term').click()
 
-    # Submit select term option
-    button_button = driver.find_element_by_xpath("/html/body/div[3]/form/input[2]").click()
+# Submit select term option
+button_button = driver.find_element_by_xpath("/html/body/div[3]/form/input[2]").click()
 
-    # Navigate to Add Drop
-    student_button = driver.find_element_by_link_text('Add or Drop Classes').click()
+# Navigate to Add Drop
+student_button = driver.find_element_by_link_text('Add or Drop Classes').click()
 
+
+while counter <= 3:
     # Enter CRN numbers, adjust keys accordingly
     id_box = driver.find_element_by_id("crn_id1")
     id_box.send_keys(CRN1)
@@ -71,4 +72,9 @@ while counter < 3:
     print("Script Completed")
     if driver.page_source.find("Registration Add Errors"):
         counter = counter+1
-print("An error has occurred 3 times: Moving to do alternative schedule")
+
+if counter == 3:
+    print("Program has failed 3 times")
+else:
+    print("Program has succeeded")
+driver.close()
